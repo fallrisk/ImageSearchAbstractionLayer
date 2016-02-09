4b88a3efe1285987249e5aa97c4564a9c3d7f681
Justin Watson <jwatson5@gmail.com>
2016-02-08 16:17:04 -0800

    Got the Babel File Watcher working. I have debugging working. My API path "/api/imagesearch/:search" is working.

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # Image Search Abstraction Layer
+http://www.freecodecamp.com/challenges/image-search-abstraction-layer
+
+I used the [Imgur API](http://api.imgur.com/).
 
 ## User Stories (Requirements)
 * User Story: I can get the image URLs, alt text and page urls for a set of images relating to a given search string.
 * User Story: I can paginate through the responses by adding a ?offset=2 parameter to the URL
 * User Story: I can get a list of the most recently submitted search strings.
+
+## Setup
+This project was done in Webstorm 11.0.3 with babel-cli set as a file watcher.
--- app.js
+++ app.js
@@ -1,22 +1,49 @@
-var express = require('express');
-var logger = require('morgan');
-var path = require('path');
-var router = express.Router();
-var timestamp = require("./timestamp")
-var RSVP = require("rsvp")
-var Promise = RSVP.Promise
-var fs = require("fs")
-var path = require("path")
-var marked = require("marked")
+import express from 'express'
+import logger from 'morgan'
+import Promise from 'bluebird'
+import fs from 'fs'
+import path from 'path'
+import marked from 'marked'
 
-var app = express();
-app.use(express.static(path.join(__dirname, 'public')));
+var debug = require('debug')('Tally:app');
+var router = express.Router()
+
+var app = express()
+app.use(express.static(path.join(__dirname, 'public')))
 app.use(logger('dev'))
 
+//
 // Routes
-////////////////////////////////////////////////////////////////////////////////
+// -----------------------------------------------------------------------------
 
-router.get("/", function(req, res, next){
+router.get('/api', (req, res) => {
+  res.status(200).json({
+    apiVersion: "1.0"
+  })
+})
+
+router.get('/api/imagesearch/:search', (req, res, next) => {
+  if (!('search' in req.params)) {
+    res.status(200).send('NOT OK');
+  }
+
+  if (req.params.search === '') {
+    res.status(200).send('NOT OK');
+  }
+
+  let searchQuery = req.params.search;
+
+  debug('search query = ' + searchQuery);
+
+  if ('offset' in req.query) {
+    let offset = req.query.offset;
+    debug('offset = ' + offset);
+  }
+
+  res.status(200).send('OK');
+});
+
+router.get("/", (req, res, next) => {
   // Render the README file.
   var p = new Promise(function(res, rej){
     fs.readFile("README.md", "utf8", function(err, data) {
@@ -26,33 +53,35 @@ router.get("/", function(req, res, next){
   }).then(function(val){
     res.status(200).send(marked(val.toString()))
   })
+  return p
 })
 
 app.use(router)
 
+//
 // Error Handlers
-////////////////////////////////////////////////////////////////////////////////
+// -----------------------------------------------------------------------------
 
 // Development Error Handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function (err, req, res, next) {
-        res.status(err.status || 500);
+    app.use((err, req, res, next) => {
+        res.status(err.status || 500)
         res.render('error', {
             message: err.message,
             error: err
-        });
-    });
+        })
+    })
 }
 
 // Production Error Handler
 // no stacktraces leaked to user
-app.use(function (err, req, res, next) {
-    res.status(err.status || 500);
+app.use((err, req, res, next) => {
+    res.status(err.status || 500)
     res.render('error', {
         message: err.message,
         error: {}
-    });
-});
+    })
+})
 
 module.exports = app;
